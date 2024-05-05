4c4cc63822c3038efe5f8a7970544e5dd422a44d
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-05 03:15:55 -0300

    Preparação para rodar com o docker-compose

--- src/main/java/br/com/bluesburguer/orderingsystem/order/infra/client/EventPublisher.java
+++
@@ -1,8 +0,0 @@
-package br.com.bluesburguer.orderingsystem.order.infra.client;
-
-import br.com.bluesburguer.orderingsystem.domain.event.OrderStatusUpdated;
-
-public interface EventPublisher {
-
-	void publish(OrderStatusUpdated event);
-}
