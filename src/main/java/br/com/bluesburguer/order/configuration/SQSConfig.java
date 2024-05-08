6fdc5227ae83568530463ed873e0cdb51c963481
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-07 21:53:44 -0300

    Adequações para rodar utilizando localstack

--- src/main/java/br/com/bluesburguer/order/configuration/SQSConfig.java
+++ src/main/java/br/com/bluesburguer/order/configuration/SQSConfig.java
@@ -1,16 +1,67 @@
 package br.com.bluesburguer.order.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 
 @Configuration
 public class SQSConfig {
 
+	@Value("${cloud.aws.endpoint.uri}")
+	private String host;
+
+	@Value("${cloud.aws.credentials.access-key:key}")
+	private String accessKeyId;
+
+	@Value("${cloud.aws.credentials.secret-key:value}")
+	private String secretAccessKey;
+
+	@Value("${cloud.aws.region.static:us-east-1}")
+	private String region;
+
+	// Configurações dos beans para o SNS e SQS
 	@Bean
-	AmazonSQS amazonSqs() {
-		return AmazonSQSClientBuilder.defaultClient();
+	@Primary
+	NotificationMessagingTemplate notificationMessagingTemplate(AmazonSNS amazonSNS) {
+		return new NotificationMessagingTemplate(amazonSNS);
+	}
+
+	@Bean
+	@Primary
+	AmazonSQSAsync amazonSQSAsync() {
+		return AmazonSQSAsyncClientBuilder.standard().withEndpointConfiguration(getEndpointConfiguration())
+				.withCredentials(getCredentialsProvider()).build();
+	}
+
+	@Bean
+	@Primary
+	AmazonSNS amazonSNSAsync() {
+		return AmazonSNSAsyncClientBuilder.standard().withEndpointConfiguration(getEndpointConfiguration())
+				.withCredentials(getCredentialsProvider()).build();
+	}
+
+	@Bean
+	@Primary
+	QueueMessagingTemplate queueMessagingTemplate() {
+		return new QueueMessagingTemplate(amazonSQSAsync());
+	}
+
+	private EndpointConfiguration getEndpointConfiguration() {
+		return new EndpointConfiguration(host, region);
+	}
+
+	public AWSStaticCredentialsProvider getCredentialsProvider() {
+		return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKeyId, secretAccessKey));
 	}
 }
