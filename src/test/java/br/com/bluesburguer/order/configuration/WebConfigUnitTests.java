c2831d07550526a6f5e8b007c1312421c4da2b3d
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-11 17:58:42 -0300

    Melhoria nos testes de WebConfig

--- src/test/java/br/com/bluesburguer/order/configuration/WebConfigUnitTests.java
+++ src/test/java/br/com/bluesburguer/order/configuration/WebConfigUnitTests.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties.Discovery;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
 import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
 import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
@@ -48,9 +51,78 @@ class WebConfigUnitTests {
 	private WebConfig webConfig;
 	
 	@Test
-	void shouldConfigureWebMvcEndpointHandlerMapping() {
+	void givenDiscoveryEnabledAndTruthyBasePath_WhenRunCustomizedConfiguration_ThenShouldResolveEndpointLinks() {
 		
 		doReturn(true)
+			.when(discovery).isEnabled();
+		
+		doReturn(discovery)
+			.when(webEndpointProperties).getDiscovery();
+		
+		doReturn("/")
+			.when(webEndpointProperties).getBasePath();
+		
+		var handlerMapping = webConfig.webEndpointServletHandlerMapping(
+				webEndpointsSupplier, servletEndpointsSupplier, 
+				controllerEndpointsSupplier, endpointMediaTypes, 
+				corsProperties, webEndpointProperties, environment);
+		
+		assertThat(handlerMapping)
+			.isNotNull();
+	}
+	
+	@Test
+	void givenDiscoveryEnabledAndNullBasePath_WhenRunCustomizedConfiguration_ThenShouldResolveEndpointLinks() {
+		
+		doReturn(true)
+			.when(discovery).isEnabled();
+		
+		doReturn(discovery)
+			.when(webEndpointProperties).getDiscovery();
+		
+		doReturn(null)
+			.when(webEndpointProperties).getBasePath();
+		
+		var handlerMapping = webConfig.webEndpointServletHandlerMapping(
+				webEndpointsSupplier, servletEndpointsSupplier, 
+				controllerEndpointsSupplier, endpointMediaTypes, 
+				corsProperties, webEndpointProperties, environment);
+		
+		assertThat(handlerMapping)
+			.isNotNull();
+	}
+	
+	@Test
+	void givenDiscoveryEnabledAndDifferentManagementPort_WhenRunCustomizedConfiguration_ThenShouldResolveEndpointLinks() {
+		
+		doReturn(true)
+			.when(discovery).isEnabled();
+		
+		doReturn(discovery)
+			.when(webEndpointProperties).getDiscovery();
+		
+		doReturn(null)
+			.when(webEndpointProperties).getBasePath();
+		
+		try (MockedStatic<ManagementPortType> utilities = Mockito.mockStatic(ManagementPortType.class)) {
+			utilities
+				.when(() -> ManagementPortType.get(environment))
+				.thenReturn(ManagementPortType.DIFFERENT);
+			
+			var handlerMapping = webConfig.webEndpointServletHandlerMapping(
+					webEndpointsSupplier, servletEndpointsSupplier, 
+					controllerEndpointsSupplier, endpointMediaTypes, 
+					corsProperties, webEndpointProperties, environment);
+			
+			assertThat(handlerMapping)
+				.isNotNull();
+		}
+	}
+	
+	@Test
+	void givenDiscoveryDisabled_WhenRunCustomizedConfiguration_ThenShouldResolveEndpointLinks() {
+		
+		doReturn(false)
 			.when(discovery).isEnabled();
 		
 		doReturn(discovery)
