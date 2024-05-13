37cb0a4ea6a3d843da91c8908b0dfaeff93ca72f
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-13 17:44:17 -0300

    Primeira versão que o http://127.0.0.1:8000/swagger-ui/index.html abre, porém o http://127.0.0.1:8000/v3/api-docs está com uma lista de componentes vazia

--- src/main/java/br/com/bluesburguer/order/BluesBurguerOrderApplication.java
+++ src/main/java/br/com/bluesburguer/order/BluesBurguerOrderApplication.java
@@ -3,12 +3,14 @@ package br.com.bluesburguer.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /** 
  * 
 */
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = { "br.com.blusburguer" })
 public class BluesBurguerOrderApplication {
 	
 	public static void main(String[] args) {
